f622846b445ad11534ee0881e8a98ec1782e5e04
Nicolas LaCasse <nlacasse@google.com>
2014-09-05 15:01:00 -0700

    Improvements to the playground bundler: * Support nested directories (needed for go examples). * Support index comments, so we can order the examples in the   playground.
    Change-Id: I58e140299c6d17da3ccf68fe63d7c77cc34f3332

--- client/playground/bundler/index.js
+++ client/playground/bundler/index.js
@@ -27,7 +27,7 @@ function run(){
 
   // Loop over each path.
   _.each(dirs, function(dir){
-    var subFiles = glob.sync('*', {
+    var subFiles = glob.sync('**', {
       cwd: dir,
       mark: true // Add a '/ character to directory matches
     });
@@ -40,26 +40,65 @@ function run(){
 
     // Loop over each subfile in the path.
     _.each(subFiles, function(fileName) {
-      // Ignore directories.
-      if (_.last(fileName) === '/') {
-        return;
-      }
-
-      // Ignore a previously bundled file.
-      if (fileName === BUNDLE_NAME) {
+      if (shouldIgnore(fileName)) {
         return;
       }
 
       var fullFilePath = path.resolve(dir, fileName);
+      var text = fs.readFileSync(fullFilePath, { encoding: 'utf8' });
+
+      var indexAndText = getIndex(text);
+      var index = indexAndText.index;
+      text = indexAndText.text;
+
       out.files.push({
-        name: fileName,
-        text: fs.readFileSync(fullFilePath, { encoding: 'utf8' })
+        name: path.basename(fileName),
+        text: text,
+        index: index
       });
     });
+
+    out.files = _.sortBy(out.files, 'index');
 
     // Write the bundle.json.
     var outFile = path.resolve(dir, BUNDLE_NAME);
     fs.writeFileSync(outFile, JSON.stringify(out));
     console.log('Wrote ' + outFile);
   });
+}
+
+// If the first line is an index comment, strip the line from text and return
+// the index and stripped text.
+function getIndex(text) {
+  var index = null;
+  var lines = text.split('\n');
+  var match = lines[0].match(/^\/\/\s*index=(\d+)/);
+  if (match && match[1]) {
+    index = match[1];
+    // Remove the first line from text.
+    text = _.rest(lines).join('\n');
+  }
+  return {
+    index: index,
+    text: text
+  };
+}
+
+function shouldIgnore(fileName) {
+  // Ignore directories.
+  if (_.last(fileName) === '/') {
+    return true;
+  }
+
+  // Ignore bundle files.
+  if (fileName === BUNDLE_NAME) {
+    return true;
+  }
+
+  // Ignore generated .vdl.go files.
+  if ((/\.vdl\.go$/i).test(fileName)) {
+    return true;
+  }
+
+  return false;
 }
