78f2245d7904c085ab5bbffcdf4f7fb6e49bee53
Adam Sadovsky <sadovsky@google.com>
2014-10-23 18:50:04 -0700

    playground: comments & tweaks to update.sh; make monitor.py lint-happy
    Change-Id: Ia4d0b1a651cf0ec0712c219d52e561b2be173e5c

--- compilerd/update.sh
+++ compilerd/update.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Script to rebuild and deploy the docker image and compiler to the playground
-# backends.
+# Script to rebuild and deploy compilerd and the Docker image (builder) to the
+# playground backends.
 #
 # Usage:
-#     gcutil ssh --project google.com:veyron playground-master
-#     sudo su - veyron
-#     veyron project update
-#     bash $VEYRON_ROOT/go/src/veyron.io/playground/compilerd/update.sh
+#   gcutil ssh --project google.com:veyron playground-master
+#   sudo su - veyron
+#   veyron update
+#   bash $VEYRON_ROOT/veyron/go/src/veyron.io/playground/compilerd/update.sh
 
 set -e
-readonly DATE=$(date +%F)
+set -u
+
+readonly DATE=$(date +"%Y%m%d-%H%M%S")
 readonly DISK="pg-data-${DATE}"
 
 function unmount() {
   sudo umount /mnt
-  gcloud compute --project "google.com:veyron" instances detach-disk --disk="$DISK" $(hostname) --zone us-central1-a
+  gcloud compute --project "google.com:veyron" instances detach-disk --disk="${DISK}" $(hostname) --zone us-central1-a
 }
 
 function cleanup() {
   if [[ -e /mnt/compilerd ]]; then
+    # The disk is still mounted on the master, which means it's not yet mounted
+    # on any backends. It's safe to unmount and delete it.
     unmount
-    # if the disk was still mounted, nothing is using it so go ahead
-    # and delete it.
     gcloud compute --project "google.com:veyron" disks delete "${DISK}" --zone "us-central1-a"
   fi
   sudo docker rm "${DISK}" || true
@@ -36,7 +38,7 @@ function main() {
   fi
 
   gcloud compute --project "google.com:veyron" disks create "${DISK}" --size "200" --zone "us-central1-a" --source-snapshot "pg-data-20140702" --type "pd-standard"
-  gcloud compute --project "google.com:veyron" instances attach-disk --disk="$DISK" $(hostname) --zone us-central1-a
+  gcloud compute --project "google.com:veyron" instances attach-disk --disk="${DISK}" $(hostname) --zone us-central1-a
   sudo mount /dev/sdb1 /mnt
 
   # Build the docker image.
@@ -48,8 +50,14 @@ function main() {
   # Export the docker image to disk.
   sudo docker save -o /mnt/playground.tar.gz playground
 
+  # TODO(sadovsky): We should run the playground with real input and make sure
+  # it works (produces the expected output).
+
   # Copy the compilerd binary from the docker image to the disk.
-  sudo docker run --name=${DISK} playground || true
+  # NOTE(sadovsky): The purpose of the following line is to create a container
+  # out of the docker image, so that we can copy out the compilerd binary.
+  # Annoyingly, the only way to create the container is to run the image.
+  sudo docker run --name=${DISK} playground &> /dev/null || true
   sudo docker cp ${DISK}:/usr/local/veyron/veyron/go/bin/compilerd /tmp
   sudo mv /tmp/compilerd /mnt/compilerd
   sudo docker rm ${DISK}
@@ -63,7 +71,7 @@ function main() {
   gcloud preview replica-pools --zone=us-central1-a update-template --template=pool_template.json playground-pool
   git checkout -- pool_template.json
 
-  # Rolling restart of the replicas.
+  # Perform a rolling restart of all the replicas.
   INSTANCES=$(gcloud preview replica-pools --zone=us-central1-a replicas --pool=playground-pool list|grep name:|cut -d: -f2)
   for i in ${INSTANCES}; do
     gcloud preview replica-pools --zone=us-central1-a replicas --pool=playground-pool restart ${i}
--- monitor.py
+++ monitor.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python2.7
 
-# This needs to run on a gce vm with the replica pool
-# service account scope (https://www.googleapis.com/auth/ndev.cloudman).
-#
-# You also need to enable preview in gcloud:
-# $ gcloud components update preview
-#
-# Then add it to your crontab, e.g.
-# */10 * * * * gcloud preview replica-pools --zone us-central1-a replicas --pool playground-pool list|monitor.py
+"""Playground GCE monitoring script.
 
-import os
+This needs to run on a GCE VM with the replica pool service account scope
+(https://www.googleapis.com/auth/ndev.cloudman).
+
+You also need to enable preview in gcloud:
+$ gcloud components update preview
+
+Then add it to your crontab, e.g.
+*/10 * * * * gcloud preview replica-pools --zone us-central1-a replicas --pool playground-pool list|monitor.py
+"""
+
 import datetime
 import subprocess
 import sys
@@ -17,48 +19,51 @@ import yaml
 
 DESIRED = 2
 MAX_ALIVE_MIN = 60
-POOL = "playground-pool"
-
-def runCommand(*args):
-    cmd = ['gcloud', 'preview', 'replica-pools', '--zone', 'us-central1-a']
-    cmd.extend(args)
-    subprocess.check_call(cmd)
-
-def resizePool(size):
-    runCommand("resize", "--new-size", str(size), POOL)
+POOL = 'playground-pool'
 
 
-def shouldRestart(replica):
-    if replica['status']['state'] == 'PERMANENTLY_FAILING':
-        print "replica %s failed: %s" % (replica['name'], replica['status']['details'])
-        return True
-    return isTooOld(replica)
+def RunCommand(*args):
+  cmd = ['gcloud', 'preview', 'replica-pools', '--zone', 'us-central1-a']
+  cmd.extend(args)
+  subprocess.check_call(cmd)
 
 
-def isTooOld(replica):
-    start_text = replica['status']['vmStartTime']
-    if start_text:
-        start = yaml.load(start_text)
-        uptime = datetime.datetime.now() - start
-        return uptime.seconds > MAX_ALIVE_MIN * 60
+def ResizePool(size):
+  RunCommand('resize', '--new-size', str(size), POOL)
 
 
-def restartReplica(replica):
-    print "Restarting replica " + replica['name']
-    resizePool(DESIRED + 1)
-    runCommand("replicas", "--pool", POOL, "delete", replica['name'])
+def ShouldRestart(replica):
+  if replica['status']['state'] == 'PERMANENTLY_FAILING':
+    print 'Replica %s failed: %s' % (
+        replica['name'], replica['status']['details'])
+    return True
+  return IsTooOld(replica)
 
 
-def maybeRestartReplica(replica):
-    if shouldRestart(replica):
-        restartReplica(replica)
+def IsTooOld(replica):
+  start_text = replica['status']['vmStartTime']
+  if start_text:
+    start = yaml.load(start_text)
+    uptime = datetime.datetime.now() - start
+    return uptime.seconds > MAX_ALIVE_MIN * 60
+
+
+def RestartReplica(replica):
+  print 'Restarting replica ' + replica['name']
+  ResizePool(DESIRED + 1)
+  RunCommand('replicas', '--pool', POOL, 'delete', replica['name'])
+
+
+def MaybeRestartReplica(replica):
+  if ShouldRestart(replica):
+    RestartReplica(replica)
 
 
 def main():
-    replicas = yaml.load_all(sys.stdin.read())
-    for replica in replicas:
-        maybeRestartReplica(replica)
+  replicas = yaml.load_all(sys.stdin.read())
+  for replica in replicas:
+    MaybeRestartReplica(replica)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  main()
