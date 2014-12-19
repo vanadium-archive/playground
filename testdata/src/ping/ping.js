64d3b7bea5f73124cb43c93e5836365d10070160
Matt Rosencrantz <mattr@google.com>
2014-12-19 09:51:34 -0800

    Playground: Fix the playground shell-test by passing a context.
    Change-Id: If212f5f5041fab67fae6bd4bdb600324fd243ad3

--- testdata/src/ping/ping.js
+++ testdata/src/ping/ping.js
@@ -4,10 +4,10 @@ var context = veyron.context;
 veyron.init(function(err, rt){
   if (err) throw err;
 
-  rt.bindTo('pingpong', function(err, s){
-    if (err) throw err;
+  var ctx = new context.Context();
 
-    var ctx = new context.Context();
+  rt.bindTo(ctx, 'pingpong', function(err, s){
+    if (err) throw err;
 
     s.ping(ctx, 'PING', function(err, pong){
       if (err) throw err;
