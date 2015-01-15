8721ff2ec0ca5efd5fef916d302650b43d2c8f23
Ivan Pilat <ivanpi@google.com>
2015-01-15 10:20:59 -0800

    playground: Refactored playground test script.
    Separated initialization and run code duplicated in veyron-www
    playground-test into sh library.
    
    Change-Id: Iaf4fc5f9fba9ab928c53bc9573c5bd0e46250505

--- test.sh
+++ test.sh
@@ -2,50 +2,14 @@
 
 # Tests the playground builder tool.
 
-# TODO(sadovsky): Much of the setup code below also exists in
-# veyron-www/test/playground_test.sh.
-
 source "$(go list -f {{.Dir}} v.io/core/shell/lib)/shell_test.sh"
-
-# Installs the release/javascript/core library and makes it accessible to javascript files in
-# the veyron playground test folder under the module name 'veyron'.
-install_veyron_js() {
-  # TODO(nlacasse): Once release/javascript/core is publicly available in npm, replace this
-  # with "npm install veyron".
-
-  pushd "${VANADIUM_ROOT}/release/javascript/vom"
-  npm link
-  popd
-  pushd "${VANADIUM_ROOT}/release/javascript/core"
-  npm link vom
-  npm link
-  popd
-  npm link veyron
-}
-
-# Installs the pgbundle tool.
-install_pgbundle() {
-  pushd "${VANADIUM_ROOT}/release/javascript/pgbundle"
-  npm link
-  popd
-  npm link pgbundle
-}
-
-# Installs various go binaries.
-build_go_binaries() {
-  shell_test::build_go_binary 'v.io/core/veyron/tools/principal'
-  shell_test::build_go_binary 'v.io/core/veyron/services/proxy/proxyd'
-  shell_test::build_go_binary 'v.io/core/veyron/services/mounttable/mounttabled'
-  shell_test::build_go_binary 'v.io/core/veyron2/vdl/vdl'
-  shell_test::build_go_binary 'v.io/playground/builder'
-  shell_test::build_go_binary 'v.io/wspr/veyron/services/wsprd'
-}
+source "$(go list -f {{.Dir}} v.io/playground)/lib/pg_test_util.sh"
 
 # Sets up a directory with the given files, then runs builder.
 test_with_files() {
   local -r TESTDATA_DIR="$(go list -f {{.Dir}} v.io/playground)/testdata"
 
-  # Write input files to a fresh dir, then run pgbundle.
+  # Write input files to a fresh dir before bundling and running them.
   local -r PGBUNDLE_DIR=$(shell::tmp_dir)
   for f in $@; do
     fdir="${PGBUNDLE_DIR}/$(dirname ${f})"
@@ -53,32 +17,16 @@ test_with_files() {
     cp "${TESTDATA_DIR}/${f}" "${fdir}/"
   done
 
-  ./node_modules/.bin/pgbundle "${PGBUNDLE_DIR}"
-
-  # Create a fresh dir to run bundler from.
-  local -r ORIG_DIR=$(pwd)
-  pushd $(shell::tmp_dir)
-  ln -s "${ORIG_DIR}/node_modules" ./  # for release/javascript/core
-  "${shell_test_BIN_DIR}/builder" -v=0 --includeV23Env=true < "${PGBUNDLE_DIR}/bundle.json" 2>&1 | tee builder.out
-  # Move builder output to original dir for verification.
-  mv builder.out "${ORIG_DIR}"
-  popd
+  test_pg_example "${PGBUNDLE_DIR}" "-v=false --includeV23Env=true"
 }
 
 main() {
   cd "${shell_test_WORK_DIR}"
 
-  export GOPATH="$(pwd):$(v23 env GOPATH)"
-  export VDLPATH="$(pwd):$(v23 env VDLPATH)"
-  export PATH="$(pwd):${shell_test_BIN_DIR}:${VANADIUM_ROOT}/environment/cout/node/bin:${PATH}"
-
-  # We unset all environment variables that supply a principal in order to
-  # simulate production playground setup.
-  unset VEYRON_CREDENTIALS
-  unset VEYRON_AGENT_FD
+  setup_environment
 
   build_go_binaries
-  install_veyron_js
+  install_vanadium_js
   install_pgbundle
 
   echo -e "\n\n>>>>> Test as the same principal\n\n"
