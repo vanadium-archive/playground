d474ae4c5b48ec06d67d6dd3ff49f9a031383fb8
Jiri Simsa <jsimsa@google.com>
2015-04-21 11:12:19 -0700

    playground: adding missing copyright
    Change-Id: I60163af6d1ee1b46dc443e26252a04696aab3268

--- go/src/v.io/x/playground/deploy/monitor.py
+++ go/src/v.io/x/playground/deploy/monitor.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python2.7
+# Copyright 2015 The Vanadium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 """Playground GCE monitoring script.
 
